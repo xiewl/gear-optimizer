30b69a2f9f9c3529aeddddac63554d727400af4e
bathfire <>
2023-11-14 02:52:44 +0800

    Updates

---
+++ precache-manifest.d7fdf1e7dfb882cdf50706f80332d1f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91cec4faf09723108e48eb705000ed22",
+    "url": "/gear-optimizer/9f84066de1c6e4dbdb8e.worker.js"
+  },
+  {
+    "revision": "c5fb820157b7c82e6a128e371b28ff22",
+    "url": "/gear-optimizer/index.html"
+  },
+  {
+    "revision": "f4d3096eb74dc4c0bc04",
+    "url": "/gear-optimizer/static/css/main.6d50546c.chunk.css"
+  },
+  {
+    "revision": "461f7a3284d0ec8846f8",
+    "url": "/gear-optimizer/static/js/2.a99cf3c2.chunk.js"
+  },
+  {
+    "revision": "13a4a448a49516133006bfd9a96890a0",
+    "url": "/gear-optimizer/static/js/2.a99cf3c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d3096eb74dc4c0bc04",
+    "url": "/gear-optimizer/static/js/main.97ad3f14.chunk.js"
+  },
+  {
+    "revision": "dc1c676ba897cc436e9f",
+    "url": "/gear-optimizer/static/js/runtime-main.0d730cbe.js"
+  }
+]);
